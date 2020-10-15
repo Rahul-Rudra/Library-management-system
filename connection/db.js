e2159403b6416de0033e9ac860590b192abf4168
Rahul Pal <pal_rahul@rudrainnovative.in>
2020-10-15 14:00:11 +0530

    path add

--- connection/db.js
+++ connection/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-const mongo =
+require("dotenv").config();
+const MONGO_URL =
+  process.env.MONGO_URL ||
   "mongodb+srv://Rahul:1387rahul@cluster0.zjhbo.mongodb.net/<Notebokk>?retryWrites=true&w=majority";
 
 const connectDB = async () => {
-  await mongoose.connect(mongo, {
+  await mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
