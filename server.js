5a2a5bf4aeaa1352b449c24a0b5eea120db5861b
Rahul Pal <pal_rahul@rudrainnovative.in>
2020-10-15 12:10:53 +0530

    modified

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use("/forget-password", require("./routes/forget"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
