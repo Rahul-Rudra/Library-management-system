b65d49143fcd2ded6e92371fea6b1d9fd20a2ee5
Rahul Pal <pal_rahul@rudrainnovative.in>
2020-10-15 11:20:14 +0530

    index.html add

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use("/forget-password", require("./routes/forget"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
 }
 
 app.listen(port, (req, res) => {
